4d9a80628fdf2f1067f1bf68c106f32eecaea082
Riyaz Ul Haque <30811072+RiyazUlHaque@users.noreply.github.com>
2021-07-10 19:29:46 -0400

    New changes

--- About.py
+++ About.py
@@ -30,11 +30,13 @@ class Ui_aboutDialog(object):
         self.label.setGeometry(QtCore.QRect(10, 170, 150, 150))
         self.label.setText(_fromUtf8(""))
         self.label.setPixmap(QtGui.QPixmap(_fromUtf8("Riz.jpg")))
+        
         self.label.setObjectName(_fromUtf8("label"))
         self.label_2 = QtGui.QLabel(Dialog)
         self.label_2.setGeometry(QtCore.QRect(170, 170, 150, 150))
         self.label_2.setText(_fromUtf8(""))
         self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("YooGle.jpg")))
+        
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.label_3 = QtGui.QLabel(Dialog)
         self.label_3.setGeometry(QtCore.QRect(20, 330, 141, 21))
@@ -42,6 +44,7 @@ class Ui_aboutDialog(object):
         font.setFamily(_fromUtf8("Comic Sans MS"))
         font.setPointSize(14)
         self.label_3.setFont(font)
+        
         self.label_3.setObjectName(_fromUtf8("label_3"))
         self.label_4 = QtGui.QLabel(Dialog)
         self.label_4.setGeometry(QtCore.QRect(180, 330, 141, 21))
@@ -49,12 +52,15 @@ class Ui_aboutDialog(object):
         font.setFamily(_fromUtf8("Comic Sans MS"))
         font.setPointSize(14)
         self.label_4.setFont(font)
+        
         self.label_4.setObjectName(_fromUtf8("label_4"))
         self.label_5 = QtGui.QLabel(Dialog)
         self.label_5.setGeometry(QtCore.QRect(20, 350, 141, 16))
+        
         self.label_5.setObjectName(_fromUtf8("label_5"))
         self.label_6 = QtGui.QLabel(Dialog)
         self.label_6.setGeometry(QtCore.QRect(180, 350, 131, 16))
+        
         self.label_6.setObjectName(_fromUtf8("label_6"))
         self.label_8 = QtGui.QLabel(Dialog)
         self.label_8.setGeometry(QtCore.QRect(80, 10, 171, 31))
@@ -62,21 +68,27 @@ class Ui_aboutDialog(object):
         font.setFamily(_fromUtf8("Comic Sans MS"))
         font.setPointSize(14)
         self.label_8.setFont(font)
+        
         self.label_8.setObjectName(_fromUtf8("label_8"))
         self.label_7 = QtGui.QLabel(Dialog)
         self.label_7.setGeometry(QtCore.QRect(70, 40, 191, 21))
+        
         self.label_7.setObjectName(_fromUtf8("label_7"))
         self.label_9 = QtGui.QLabel(Dialog)
         self.label_9.setGeometry(QtCore.QRect(110, 50, 191, 31))
+        
         self.label_9.setObjectName(_fromUtf8("label_9"))
         self.label_10 = QtGui.QLabel(Dialog)
         self.label_10.setGeometry(QtCore.QRect(90, 80, 191, 31))
+        
         self.label_10.setObjectName(_fromUtf8("label_10"))
         self.label_11 = QtGui.QLabel(Dialog)
         self.label_11.setGeometry(QtCore.QRect(140, 110, 51, 21))
+        
         self.label_11.setObjectName(_fromUtf8("label_11"))
         self.label_12 = QtGui.QLabel(Dialog)
         self.label_12.setGeometry(QtCore.QRect(140, 120, 51, 31))
+        
         self.label_12.setObjectName(_fromUtf8("label_12"))
         self.label_13 = QtGui.QLabel(Dialog)
         self.label_13.setGeometry(QtCore.QRect(80, 370, 201, 21))
@@ -84,8 +96,8 @@ class Ui_aboutDialog(object):
         font.setFamily(_fromUtf8("Cambria Math"))
         font.setPointSize(12)
         self.label_13.setFont(font)
+        
         self.label_13.setObjectName(_fromUtf8("label_13"))
-
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
